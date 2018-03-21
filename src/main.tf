aca5bb99e11976e8c1f674dfc78f0994bea4bc4a
Mikael Krief <krief_mikael@hotmail.com>
2018-03-21 10:13:33 +0100

    del tag

--- src/main.tf
+++ src/main.tf
@@ -55,10 +55,6 @@ resource "azurerm_subnet" "subnet" {
   virtual_network_name = "${azurerm_virtual_network.vnet.name}"
   resource_group_name  = "${azurerm_resource_group.rg.name}"
   address_prefix       = "10.0.10.0/24"
-
-  tags {
-    environment = "Terraform HOL bootcamp"
-  }
 }
 
 resource "azurerm_network_interface" "nic" {
